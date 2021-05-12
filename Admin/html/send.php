bd9576cdc8a2ca35d2bfb98b34d19c7677d57978
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 06:27:06 +0900

    入力した情報でプッシュ通知を投げる処理を実装

---
+++ Admin/html/send.php
@@ -0,0 +1,27 @@
+<?php
+    $title = $_POST["title"];
+    $message = $_POST["message"];
+    if (strlen($title) == 0 || strlen($message) == 0)
+    {
+        echo "Error.";
+        return;
+    }
+
+    $api_endpoint = "https://6sf41e1u56.execute-api.ap-northeast-1.amazonaws.com/default/publish_push_notification";
+    $send_data = array(
+        "title" => $title,
+        "message" => $message
+    );
+    $send_json = json_encode($send_data);
+    
+    $context = array(
+        "http" => array(
+            "method" => "POST",
+            "header" => "Content-Type: application/json; charset=UTF-8",
+            "content" => $send_json
+        )
+    );
+
+    $result = file_get_contents($api_endpoint, false, stream_context_create($context));
+    header("Location: index.html");
+?>
