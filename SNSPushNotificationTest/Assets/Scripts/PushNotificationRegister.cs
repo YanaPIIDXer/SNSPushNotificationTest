212694c749a1633f49e655a4c4203e77189360a9
YanaPIIDXer <>
2021-05-13 01:06:27 +0900

    プッシュ通知登録用クラスをとりあえず定義

---
+++ SNSPushNotificationTest/Assets/Scripts/PushNotificationRegister.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プッシュ通知登録
+/// </summary>
+public class PushNotificationRegister : MonoBehaviour
+{
+    /// <summary>
+    /// リージョン
+    /// </summary>
+    private static readonly string Region = "ap-northeast-1";
+
+    /// <summary>
+    /// IDプールのID
+    /// </summary>
+    private static readonly string IdentityPoolId = "ap-northeast-1:07e20c63-b62e-4868-bcba-19400190ede2";
+
+    /// <summary>
+    /// SNSアプリケーションのARN
+    /// </summary>
+    private static readonly string ApplicationArn = "arn:aws:sns:ap-northeast-1:310815347645:app/APNS_SANDBOX/Test";
+}
